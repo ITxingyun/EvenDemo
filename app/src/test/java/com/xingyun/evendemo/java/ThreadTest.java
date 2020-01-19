08af9199be71345d316a8d23b4b7581df6efef5b
bachen <bachen@xogrp.com>
2020-01-19 15:18:27 +0800

    多线程

---
+++ app/src/test/java/com/xingyun/evendemo/java/ThreadTest.java
@@ -0,0 +1,14 @@
+package com.xingyun.evendemo.java;
+
+import org.junit.Test;
+
+/***
+ * 多线程demo
+ */
+public class ThreadTest {
+
+    @Test
+    public void testThread() {
+        new Thread().start();
+    }
+}
