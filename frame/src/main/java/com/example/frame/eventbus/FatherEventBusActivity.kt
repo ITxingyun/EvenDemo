8196a9c6e60e234b2abaff09a6ba80406226e930
bachen <bachen@xogrp.com>
2020-07-23 18:33:07 +0800

    EventBus

--- frame/src/main/java/com/example/frame/eventbus/FatherEventBusActivity.kt
+++ frame/src/main/java/com/example/frame/eventbus/FatherEventBusActivity.kt
@@ -8,15 +8,31 @@ import org.greenrobot.eventbus.EventBus
 import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.ThreadMode
 
-class EventBusActivity : FatherEventBusActivity() {
+open class FatherEventBusActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_event_bus)
     }
 
+    override fun onStart() {
+        super.onStart()
+        EventBus.getDefault().register(this)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        EventBus.getDefault().unregister(this)
+    }
+
     @Subscribe(threadMode = ThreadMode.MAIN, sticky = true)
-    override fun onMessageEvent(event: MessageEvent) {
+    open fun onMessageEvent(event: MessageEvent) {
+        text.text = event.message
+    }
+
+
+    @Subscribe(threadMode = ThreadMode.MAIN, sticky = true)
+    fun onMessageEvent1(event: MessageEvent) {
         text.text = event.message
     }
 
